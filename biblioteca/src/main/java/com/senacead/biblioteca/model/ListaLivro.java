e191a1b1189af5c3186e665a2650bc4d1802ccbc
Lucas Fraga <ti.lucasfraga@gmail.com>
2023-10-03 18:51:22 -0300

    Adicionado comentário na classe ListaLivro

--- biblioteca/src/main/java/com/senacead/biblioteca/model/ListaLivro.java
+++ biblioteca/src/main/java/com/senacead/biblioteca/model/ListaLivro.java
@@ -3,6 +3,9 @@ package com.senacead.biblioteca.model;
 import com.senacead.biblioteca.model.entity.LivroEntity;
 import java.util.ArrayList;
 
+// Classe criada para guardar informações na memória
+// Como estamos gravando os dados no banco de dados, ela não é mais utilizada
+
 public class ListaLivro {
     private static ArrayList<LivroEntity> lista = new ArrayList<LivroEntity>();
     
